f9aeb106afb57c856b9f6c7ce9a0ff664c4fb8c6
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2025-08-05 15:39:01 -0400

    Update and rename src/Tweet.java to Post.java

---
+++ Post.java
@@ -0,0 +1,16 @@
+class Post{
+   String postAuthor;
+   String postContent;
+   int postLikes;
+
+  void likePost(){
+      postLikes++;
+      System.out.println("You liked this.";
+   }
+
+   public String toString(){
+     return p.postAuthor + " posted \n" 
+           + p.postContent + "\n" 
+           + p.postLikes + " likes";
+   }
+}
