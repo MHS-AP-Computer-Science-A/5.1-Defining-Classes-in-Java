3181046be6784c4f196286c910ce0c38d965f121
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2025-08-05 15:41:15 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,27 +2,16 @@
 public class Main {
 
 	public static void main(String[] args) {
-		//Create a Tweet object
-		Tweet t = new Tweet();
+		Post p = new Post();
 
-		//Set the instance variables
-		t.text = "Avocados are squishy";
-		t.author = "Mr. Fox";
-		t.likes = 100;
+		p.postContent = "Avocados are squishy";
+		p.postAuthor = "Mr. Fox";
+		p.likePost();
+		p.likePost();
+		
+		System.out.println(p.postAuthor + " posted \n" + p.postContent + "\n" + p.postLikes + " likes");
 
-		//Display the  Tweet
-		System.out.println(t.author + " tweeted \n" + t.text + "\n" + t.likes + " likes\n");
-		
-		//Use the like method to like the Tweet
-		t.like();
-		
-		//Display the Tweet again, but this time with the toString method
-		//Notice the likes have increased by 1
-		System.out.println(t);
-		
-		//Add a reply and display again
-		t.addReply("I'll bring the tacos", "Dave");
-		System.out.println("\n" + t);
 	}
 
 }
+
